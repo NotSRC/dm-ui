f276b30eb866cd0ab2bceb321934ffa9e5c570ef
Dima Tsourkan <dimatsourkan@gmail.com>
2020-09-25 17:29:17 +0300

    0.0.12

--- projects/ui/src/lib/services/api/api.service.ts
+++ projects/ui/src/lib/services/api/api.service.ts
@@ -1,69 +1,66 @@
 import { HttpClient } from '@angular/common/http';
 import { Params } from '@angular/router';
-import { Injectable } from '@angular/core';
 
-
-@Injectable()
 export abstract class ApiService {
   protected abstract debug: boolean;
   protected abstract apiUrl: string;
 
-  constructor(private http: HttpClient) {
+  constructor(protected http: HttpClient) {
   }
 
-  async get(url: string, params: Params = {}): Promise<any> {
+  get(url: string, params: Params = {}) {
     if (this.debug) {
       console.log('get', url, params);
     }
-    const res = await this.http
-      .get(`${this.apiUrl}/${url}`, { params })
+    const res = this.http
+      .get(`${this.apiUrl}/${url}`, {params});
     if (this.debug) {
       console.log(res);
     }
     return res;
   }
 
-  async post(url: string, data: any): Promise<any> {
+  post(url: string, data: any) {
     if (this.debug) {
       console.log('post', url, data);
     }
-    const res = await this.http.post(`${this.apiUrl}/${url}`, data).toPromise();
+    const res = this.http.post(`${this.apiUrl}/${url}`, data).toPromise();
     if (this.debug) {
       console.log(res);
     }
     return res;
   }
 
-  async patch(url: string, data: any): Promise<any> {
+  patch(url: string, data: any) {
     if (this.debug) {
       console.log('patch', url, data);
     }
-    const res = await this.http
-      .patch(`${this.apiUrl}/${url}`, data)
+    const res = this.http
+      .patch(`${this.apiUrl}/${url}`, data);
     if (this.debug) {
       console.log(res);
     }
     return res;
   }
 
-  async delete(url: string, params: Params = {}): Promise<any> {
+  delete(url: string, params: Params = {}) {
     if (this.debug) {
       console.log('delete', url);
     }
-    const res = await this.http
-      .delete(`${this.apiUrl}/${url}`, { params })
+    const res = this.http
+      .delete(`${this.apiUrl}/${url}`, {params});
     if (this.debug) {
       console.log(res);
     }
     return res;
   }
 
-  async getSingle(url: string, params: Params = {}): Promise<any> {
+  getSingle(url: string, params: Params = {}) {
     if (this.debug) {
       console.log('getSingle', url);
     }
-    const res = await this.http
-      .get(`${this.apiUrl}/${url}`, { params })
+    const res = this.http
+      .get(`${this.apiUrl}/${url}`, {params});
     if (this.debug) {
       console.log(res);
     }
