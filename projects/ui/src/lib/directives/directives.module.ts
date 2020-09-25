7b99383fce6983711c67e1d6d0560225ed958699
Dima Tsourkan <dimatsourkan@gmail.com>
2020-09-25 17:59:11 +0300

    0.0.15

--- projects/ui/src/lib/directives/directives.module.ts
+++ projects/ui/src/lib/directives/directives.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MaskDirective } from './mask/mask.directive';
 import { DecimalMaskDirective } from './decimal-mask/decimal-mask.directive';
 import { IntegerMaskDirective } from './integer-mask/integer-mask.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MaskDirective, DecimalMaskDirective, IntegerMaskDirective],
   exports: [MaskDirective, DecimalMaskDirective, IntegerMaskDirective],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class DmDirectivesModule {}
