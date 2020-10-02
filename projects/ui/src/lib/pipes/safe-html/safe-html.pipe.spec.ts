afcfae8b14b55965384f13e556871b0f0f0dd459
Dima Tsourkan <dimatsourkan@gmail.com>
2020-10-02 16:24:37 +0300

    feat(App): Add html sanitizer

--- projects/ui/src/lib/pipes/safe-html/safe-html.pipe.spec.ts
+++ projects/ui/src/lib/pipes/safe-html/safe-html.pipe.spec.ts
@@ -1,8 +1,8 @@
-import { SafeHtmlPipe } from './cut-number.pipe';
+import { SafeHtmlPipe } from './safe-html.pipe';
 
 describe('CutNumberPipe', () => {
   it('create an instance', () => {
-    const pipe = new SafeHtmlPipe();
+    const pipe = new SafeHtmlPipe(null);
     expect(pipe).toBeTruthy();
   });
 });
