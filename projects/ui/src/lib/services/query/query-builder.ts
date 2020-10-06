86b1bd4109c2e12e5bebf242245709505740adf7
Dima Tsourkan <dimatsourkan@gmail.com>
2020-10-06 12:05:53 +0300

    feat(App): Update types

--- projects/ui/src/lib/services/query/query-builder.ts
+++ projects/ui/src/lib/services/query/query-builder.ts
@@ -1,6 +1,7 @@
 import { CrudListQuery, SortDirection } from '../../interfaces/crud-list-query';
 import { Subject } from 'rxjs';
 import { debounceTime, filter } from 'rxjs/operators';
+import { QueryStorageSaver } from './query-storage-saver';
 
 export class QueryParamsBuilder {
   private cancelChange = false;
@@ -11,15 +12,19 @@ export class QueryParamsBuilder {
       debounceTime(this.changeDebounceTime),
     );
 
-  protected queryParams: CrudListQuery = {
-    page: 1,
-    limit: 25,
-    filter: null,
-    sortField: null,
-    sortDirection: null
-  };
+  protected queryParams: CrudListQuery;
 
-  constructor(protected changeDebounceTime = 10) {
+  constructor(
+    protected changeDebounceTime = 10,
+    protected queryStorageSaver: QueryStorageSaver
+  ) {
+    this.queryParams = {
+      page: this.queryStorageSaver.getItem<number>('page') || 1,
+      limit: this.queryStorageSaver.getItem<number>('limit') || 25,
+      filter: this.queryStorageSaver.getItem<string>('filter') || null,
+      sortField: this.queryStorageSaver.getItem<string>('sortField') || null,
+      sortDirection: this.queryStorageSaver.getItem<SortDirection>('sortDirection') || null
+    }
   }
 
   protected emitChange() {
@@ -31,39 +36,50 @@ export class QueryParamsBuilder {
     return this;
   }
 
+  protected saveParam(param: string, value: number|string, saveOnSession = false) {
+  if (saveOnSession) {
+    this.queryStorageSaver.setItem(param, value);
+  }
+  }
+
   getParam<T>(key: keyof CrudListQuery): T {
     return this.queryParams[key] as any;
   }
 
-  page(page: number|string) {
+  page(page: number|string, {saveOnSession} = { saveOnSession: false }) {
     this.queryParams.page = parseInt(`${page}`, 10) || 1;
+    this.saveParam('page', this.queryParams.page, saveOnSession);
     this.emitChange();
     return this;
   }
 
-  limit(limit: number|string) {
+  limit(limit: number|string, {saveOnSession} = { saveOnSession: false }) {
     this.queryParams.limit = parseInt(`${limit}`, 10) || 25;
+    this.saveParam('limit', this.queryParams.limit, saveOnSession);
     this.emitChange();
     return this;
   }
 
-  filter(filter: string) {
+  filter(filter: string, {saveOnSession} = { saveOnSession: false }) {
     this.queryParams.filter = filter || null;
+    this.saveParam('filter', this.queryParams.filter, saveOnSession);
     this.emitChange();
     return this;
   }
 
-  sortField(sortField: string) {
+  sortField(sortField: string, {saveOnSession} = { saveOnSession: false }) {
     this.queryParams.sortField = sortField || null;
+    this.saveParam('sortField', this.queryParams.sortField, saveOnSession);
     this.emitChange();
     return this;
   }
 
-  sortDirection(sortDirection: SortDirection | 'asc' | 'desc') {
+  sortDirection(sortDirection: SortDirection | 'asc' | 'desc', {saveOnSession} = { saveOnSession: false }) {
     this.queryParams.sortDirection = sortDirection || null;
     if (!sortDirection) {
       this.queryParams.sortField = null;
     }
+    this.saveParam('sortDirection', this.queryParams.sortDirection, saveOnSession);
     this.emitChange();
     return this;
   }
--- projects/ui/src/lib/services/query/query-storage-saver.ts
+++ projects/ui/src/lib/services/query/query-storage-saver.ts
@@ -1,6 +1,6 @@
 export interface QueryStorage {
   setItem(key: string, value: string);
-  getItem(key: string): string;
+  getItem(key: string);
 }
 
 export class QueryStorageSaver {
@@ -8,11 +8,11 @@ export class QueryStorageSaver {
   readonly storageToken = 'global';
   readonly storage: QueryStorage = sessionStorage;
 
-  setItem(param: string, value: string) {
-    this.storage.setItem(`${this.storageToken}-${param}`, value);
+  setItem(param: string, value: string|number) {
+    this.storage.setItem(`${this.storageToken}-${param}`, `${value}`);
   }
 
-  getItem(param: string) {
+  getItem<T>(param: string): T {
     return this.storage.getItem(`${this.storageToken}-${param}`);
   }
 }
