f3956bb84fc090761f8d379317f363a900ad5f88
Dima Tsourkan <dimatsourkan@gmail.com>
2020-10-13 10:19:41 +0300

    feat(App): Add exclude method to query setter

--- projects/ui/src/lib/services/query/query-setter.ts
+++ projects/ui/src/lib/services/query/query-setter.ts
@@ -1,10 +1,19 @@
 export class QuerySetter {
+  private excluded: string[];
+
+  exclude(params: string[]) {
+    this.excluded = params;
+  }
+
   getQueryParams(): { [key: string]: string } {
     const queryParams = new URLSearchParams(location.search);
     const entries = queryParams['entries']();
 
     const params = {};
     for (const entry of entries) {
+      if (this.excluded?.includes(entry[0])) {
+        return;
+      }
       params[entry[0]] = entry[1];
     }
     return params;
@@ -13,6 +22,9 @@ export class QuerySetter {
   setQueryParams(query: { [key: string]: string }) {
     const queryParams = new URLSearchParams();
     Object.keys(query).forEach((key) => {
+      if (this.excluded?.includes(key)) {
+        return;
+      }
       queryParams.set(key, query[key]);
     });
     window.history.replaceState({}, '', `${location.pathname}?${queryParams}`);
