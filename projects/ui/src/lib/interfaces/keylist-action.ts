3050fb2b5b95f6e95178643fd7c069cd3808c301
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-11-11 19:09:53 +0200

    feat(App): Update filter

---
+++ projects/ui/src/lib/interfaces/keylist-action.ts
@@ -0,0 +1,10 @@
+import { QueryParams } from '@ngrx/data';
+import { Observable } from 'rxjs';
+import { BaseResult } from './base-result';
+import { KeyListModel } from './keylist-model';
+
+export interface KeyListAction {
+  getKeyList(
+    queryParams: QueryParams = {}
+  ): Observable<BaseResult<KeyListModel>>;
+}
---
+++ projects/ui/src/lib/interfaces/keylist-model.ts
@@ -0,0 +1,7 @@
+import { User } from '@shared/models/user.model';
+import { FileModel } from 'dm-ui/dist/ui';
+
+export interface KeyListModel {
+  name: string;
+  id: string;
+}
