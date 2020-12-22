0c48422f3d9c0299b5107a3ca3448e9e4c9d5d52
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-12-22 14:02:05 +0200

    feat(App): Update store

--- projects/ui/src/lib/store/collection/dm-collection.service.ts
+++ projects/ui/src/lib/store/collection/dm-collection.service.ts
@@ -1,34 +1,26 @@
 import { Injectable } from '@angular/core';
 import { EntityCollectionServiceBase } from '@ngrx/data';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { createSelector } from '@ngrx/store';
 import { Pagination } from '../models/pagination.model';
 import { map } from 'rxjs/operators';
 import { BaseEntityModel } from '../models/base-entity.model';
-import { EntityCollectionServiceElementsFactory } from '@ngrx/data/src/entity-services/entity-collection-service-elements-factory';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Injectable({
   providedIn: 'root',
 })
 export class DmCollectionService<
   T extends BaseEntityModel
 > extends EntityCollectionServiceBase<T> {
-  constructor(
-    entityName: string,
-    serviceElementsFactory: EntityCollectionServiceElementsFactory
-  ) {
-    super(entityName, serviceElementsFactory);
-    this.paginationSelector.subscribe((val) => {
-      this.pagination.next(val);
-    });
-  }
-
+  private pagination = new BehaviorSubject<Pagination>({});
   private paginationSelector = this.store.select(
     createSelector(this.selectors.selectCollection, (collection: any) => {
-      return collection.pagination;
+      this.pagination.next(collection.pagination);
+      return collection.pagination as Pagination;
     })
   );
-  private pagination = new BehaviorSubject<Pagination>({});
   public pagination$ = this.pagination.asObservable();
 
   getFromCacheById(id: string) {
