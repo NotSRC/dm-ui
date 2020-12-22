a9a7072d3dce436e8cee081e38cf0e500f44d904
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-12-22 13:12:39 +0200

    feat(App): Update store

--- projects/ui/src/lib/store/collection/dm-collection.service.ts
+++ projects/ui/src/lib/store/collection/dm-collection.service.ts
@@ -29,7 +29,7 @@ export class DmCollectionService<
   }
 
   clearCache() {
-    this.paginationSelector.clearResult();
+    this.paginationSelector.setResult({});
     this.paginationSelector.release();
     super.clearCache();
   }
