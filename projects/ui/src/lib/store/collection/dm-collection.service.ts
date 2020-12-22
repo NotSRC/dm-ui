78a81d810d3455d0cc3c8d24d24ef17fa50288da
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-12-22 13:49:19 +0200

    feat(App): Update store

--- projects/ui/src/lib/store/collection/dm-collection.service.ts
+++ projects/ui/src/lib/store/collection/dm-collection.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { EntityCollectionServiceBase } from '@ngrx/data';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { createSelector } from '@ngrx/store';
 import { Pagination } from '../models/pagination.model';
 import { map } from 'rxjs/operators';
 import { BaseEntityModel } from '../models/base-entity.model';
+import { EntityCollectionServiceElementsFactory } from '@ngrx/data/src/entity-services/entity-collection-service-elements-factory';
 
 @Injectable({
   providedIn: 'root',
@@ -12,17 +13,23 @@ import { BaseEntityModel } from '../models/base-entity.model';
 export class DmCollectionService<
   T extends BaseEntityModel
 > extends EntityCollectionServiceBase<T> {
-  private pagination: Pagination;
-  private paginationSelector = createSelector(
-    this.selectors.selectCollection,
-    (collection: any) => {
-      this.pagination = collection.pagination;
+  constructor(
+    entityName: string,
+    serviceElementsFactory: EntityCollectionServiceElementsFactory
+  ) {
+    super(entityName, serviceElementsFactory);
+    this.paginationSelector.subscribe((val) => {
+      this.pagination.next(val);
+    });
+  }
+
+  private paginationSelector = this.store.select(
+    createSelector(this.selectors.selectCollection, (collection: any) => {
       return collection.pagination;
-    }
+    })
   );
-  public pagination$: Observable<Pagination> = this.store.select(
-    this.paginationSelector
-  );
+  private pagination = new BehaviorSubject<Pagination>({});
+  public pagination$ = this.pagination.asObservable();
 
   getFromCacheById(id: string) {
     return this.entities$.pipe(
@@ -31,10 +38,7 @@ export class DmCollectionService<
   }
 
   clearCache() {
-    delete this.pagination.page;
-    delete this.pagination.pages;
-    delete this.pagination.limit;
-    delete this.pagination.total;
+    this.pagination.next({});
     super.clearCache();
   }
 }
